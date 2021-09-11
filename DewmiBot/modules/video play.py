7bc5246b8999a71e9a15f6f0ce239a447beedfc6
blackthundercoding <90509935+blackthundercoding@users.noreply.github.com>
2021-09-11 17:36:24 +0530

    Create video play.py

---
+++ DewmiBot/modules/video play.py
@@ -0,0 +1,26 @@
+｢｢  𝗛𝗲𝗹𝗽  𝗳𝗼𝗿  video play   𝗺𝗼𝗱𝘂𝗹𝗲 」」😊\𝗻
+@rangerbeatz_bot
+📚 All Command List:
+
+❍ /vplay (reply to video or file) - to stream video
+❍ /vstop - end the video streaming
+❍ /song (song name) - download song from YT
+❍ /vsong (video name) - download video from YT
+❍ /lyric (song name) - lyric scrapper
+
+🎊 FUN CMD:
+
+❍ /asupan - check it by yourself
+❍ /chika - check it by yourself
+❍ /wibu - check it by yourself
+❍ /truth - check it by yourself
+❍ /dare - check it by yourself
+
+🔰 EXTRA CMD:
+
+❍ /alive - check bot alive status
+❍ /ping - check bot ping status
+❍ /uptime - check bot uptime status
+❍ /sysinfo - check bot system information
+
+Powerd by @rangerbeatz_bot
