89dfd8ba4ff2f43d853908ed18845fa8076fdbad
blackthundercoding <90509935+blackthundercoding@users.noreply.github.com>
2021-09-12 18:03:13 +0530

    Update warns.py

--- DewmiBot/modules/warns.py
+++ DewmiBot/modules/warns.py
@@ -510,7 +510,7 @@ be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is
  👉 /warnlimit <num>*:* set the warning limit
  👉 /strongwarn <on/yes/off/no>*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
  
- @dewmibot
+ @rangerbeatz_bot
 """
 
 __mod_name__ = "Warnings ❗️"
